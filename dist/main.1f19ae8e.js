46c37c43afe6609501f4c2d0d783fd2bc91898c9
thibault-rolland-icm <thibault.rolland@icm-institute.org>
2021-11-04 18:12:46 +0100

    generated 2d files

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -42051,7 +42051,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54267" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61894" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
